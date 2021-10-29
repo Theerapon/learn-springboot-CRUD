9ae3da3fa578122110a011674f0f9a77ce66da73
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-29 18:14:59 +0700

    hadler basic rest controller with spring data rest
    - handler for basic rest

--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -1,73 +1,73 @@
-package com.luv2code.springboot.cruddemo.service;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
-import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
-import com.luv2code.springboot.cruddemo.entity.Employee;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-
-	private EmployeeRepository employeeRepository;
-	
-//	private EmployeeDAO employeeDAO;
-	
+//package com.luv2code.springboot.cruddemo.service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+//import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+//import com.luv2code.springboot.cruddemo.entity.Employee;
+//
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//
+//	private EmployeeRepository employeeRepository;
+//	
+////	private EmployeeDAO employeeDAO;
+//	
+////	@Autowired
+////	public EmployeeServiceImpl(@Qualifier("employeeDAOHibernateImpl") EmployeeDAO employeeDAO) {
+////		this.employeeDAO = employeeDAO;
+////	}
+//	
+////	@Autowired
+////	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
+////		this.employeeDAO = employeeDAO;
+////	}
+//	
 //	@Autowired
-//	public EmployeeServiceImpl(@Qualifier("employeeDAOHibernateImpl") EmployeeDAO employeeDAO) {
-//		this.employeeDAO = employeeDAO;
+//	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+//		this.employeeRepository = employeeRepository;
 //	}
-	
-//	@Autowired
-//	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
-//		this.employeeDAO = employeeDAO;
+//
+//	@Override
+//	@Transactional
+//	public List<Employee> findAll() {
+//		//return employeeDAO.findAll();
+//		return employeeRepository.findAll();
 //	}
-	
-	@Autowired
-	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
-		this.employeeRepository = employeeRepository;
-	}
-
-	@Override
-	@Transactional
-	public List<Employee> findAll() {
-		//return employeeDAO.findAll();
-		return employeeRepository.findAll();
-	}
-
-	
-
-	@Override
-	@Transactional
-	public Employee findById(int theId) {
-		//return employeeDAO.findById(theId);
-		Optional<Employee> result = employeeRepository.findById(theId);
-				
-		if (result.isPresent()) {
-			return result.get();
-		} else {
-			throw new RuntimeException("Did not find employee id " + theId);
-		}
-	}
-
-	@Override
-	@Transactional
-	public void save(Employee theEmployee) {
-		//employeeDAO.save(theEmployee);
-		employeeRepository.save(theEmployee);
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(int theId) {
-		//employeeDAO.deleteById(theId);
-		employeeRepository.deleteById(theId);
-	}
-
-}
+//
+//	
+//
+//	@Override
+//	@Transactional
+//	public Employee findById(int theId) {
+//		//return employeeDAO.findById(theId);
+//		Optional<Employee> result = employeeRepository.findById(theId);
+//				
+//		if (result.isPresent()) {
+//			return result.get();
+//		} else {
+//			throw new RuntimeException("Did not find employee id " + theId);
+//		}
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void save(Employee theEmployee) {
+//		//employeeDAO.save(theEmployee);
+//		employeeRepository.save(theEmployee);
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void deleteById(int theId) {
+//		//employeeDAO.deleteById(theId);
+//		employeeRepository.deleteById(theId);
+//	}
+//
+//}
