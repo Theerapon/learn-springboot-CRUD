0a884b797594a7691b0cea048125a7ceee357144
Theerapon <Theerapon.nanoi.185@gmail.com>
2021-10-29 17:12:24 +0700

    add new employee api

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,24 @@ public class EmployeeRestController {
 		return employeeService.findAll();
 	}
 
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee employee = employeeService.findById(employeeId);
+		
+		if (employee == null) {
+			throw new RuntimeException("Employee id not found " + employeeId);
+		}
+		
+		return employee;
+	}
+	
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+	
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
+	}
 	
 }
